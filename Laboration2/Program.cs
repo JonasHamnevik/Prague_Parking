a186baa694168429b7b33dd12614a181bbb88340
Jonas Hamnevik <jonas.hamnevik@gmail.com>
2021-09-27 15:46:10 +0200

    Add project files.

---
+++ Laboration2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboration2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
