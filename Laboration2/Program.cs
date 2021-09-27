e8e7b4e8f2ef1eafb1b83b17f0707368e3a5b980
Jonas Hamnevik <jonas.hamnevik@gmail.com>
2021-09-27 18:21:44 +0200

    Added menu

--- Laboration2/Program.cs
+++ Laboration2/Program.cs
@@ -6,7 +6,70 @@ namespace Laboration2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] parkings = new string[10];
+
+            for (int i = 1; i < parkings.Length +1; i++)
+            {
+                Console.WriteLine("{0} ", i);
+            }
+
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("1 - Add car");
+            Console.WriteLine("2 - Add MC");
+            Console.WriteLine("3 - Move vehicle");
+            Console.WriteLine("4 - Search");
+            Console.WriteLine("5 - Remove vehicle");
+            string menu = Console.ReadLine();
+
+            switch (menu)
+            {
+                case "1":
+                    addCar();
+                    break;
+                case "2":
+                    addMc();
+                    break;
+                case "3":
+                    moveVehicle();
+                    break;
+                case "4":
+                    search();
+                    break;
+                case "5":
+                    removeVehicle();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void addCar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void addMc()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void moveVehicle()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void search()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void removeVehicle()
+        {
+            throw new NotImplementedException();
         }
     }
 }
